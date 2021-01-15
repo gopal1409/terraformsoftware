97d46a83a199c5923ec9e4d281cfcd2e3c31dae6
John cena <cena@gmail.com>
2021-01-14 23:52:46 -0700

    windows ami using terraform

--- vars.tf
+++ vars.tf
@@ -2,14 +2,6 @@ variable "AWS_REGION" {
     default = "us-east-2"
 }
 
-variable "AMIS" {
-    type = map(string)
-    default = {
-        us-east-1 = "ami-0be2609ba883822ec" 
-        us-west-1 = "ami-03130878b60947df3"
-        us-east-2 = "ami-0a0ad6b70e61be944"
-    }
-}
 variable "PATH_TO_PRIVATE_KEY"{
     default = "mykey"
 }
@@ -18,5 +10,8 @@ variable "PATH_TO_PUBLIC_KEY" {
     default = "mykey.pub"
 }
 variable "INSTANCE_USERNAME" {
-    default = "ec2-user"
+    default = "Terraform"
+}
+variable "INSTANCE_PASSWORD"{
+    
 }
